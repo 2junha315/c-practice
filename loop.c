59b11ac7a2cd41ad97c197f89eebda405e5fd6f7
junha <junhaha0315@gmail.com>
2024-12-21 17:26:25 +0900

    second c commit

--- loop.c
+++ loop.c
@@ -2,7 +2,52 @@
 
 int main(void)
 {
-	printf("Hello World");
+	//printf("Hello World");
+
+	//++
+
+	/*int a = 10;
+	printf("a is %d\n", a);
+	a++;
+	printf("a is %d\n", a);
+	a++;
+	printf("a is %d\n", a);*/
+
+	//int b = 20;
+	//printf("b is %d\n", b);
+	//printf("b is %d\n", ++b);
+	//printf("b is %d\n", b++);
+	//printf("b is %d\n", b);
+
+	/*int i = 1;
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);
+	printf("hello world %d\n", i++);*/
+
+	//반복문
+	//for, while, do while
+
+	//for(선언; 조건; 증감)
+	/*for (int i = 1;i <= 10;i++)
+	{
+		printf("hello world %d\n", i);
+	}*/
+
+	//while(조건) { }
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("hello world %d\n", i++);
+	}
+
 
 
 
