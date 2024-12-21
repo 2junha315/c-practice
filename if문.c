ff0882951cf66e44ebb5f5cd47b538714a7bf720
junha <junhaha0315@gmail.com>
2024-12-21 21:44:27 +0900

    second if practice

--- if문.c
+++ if문.c
@@ -2,7 +2,65 @@
 
 int main(void)
 {
-	printf("hello world");
+	//printf("hello world");
+	/*int age;
+	printf("age? : ");
+	scanf_s("%d", &age);
+	if (age >= 8 && age<=13)
+	{
+		printf("초등학생\n");
+	}
+	else if (age>=14 && age<=16)
+	{
+		printf("중학생\n");
+	}
+	else if (age >= 17 && age <= 19)
+	{
+		printf("고등학생\n");
+	}
+	else if (age < 7)
+	{
+		printf("유아");
+	}
+	else
+	{
+		printf("성인\n");
+	}*/
+
+//break, continue
+	/*for (int i = 1;i <= 30;i++)
+	{
+		if (i >= 6)
+		{
+			printf("나머지 학생은 귀가\n");
+			break;
+		}
+		printf("%d까지 조별 발표 준비\n", i);
+	}*/
+	for (int i = 1;i <= 30;i++)
+	{
+		if (i >= 6 && i <= 10)
+		{
+			if (i == 7)
+			{
+				printf("%d번은 결석\n", i);
+				continue;
+			}
+			printf("%d번 학생은 조별 발표 준비\n", i);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 	return 0;
 }
