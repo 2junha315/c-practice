7684103288afcf5c37ba674d0f8398fd32fb6227
junha <junhaha0315@gmail.com>
2024-12-22 16:32:06 +0900

    random project

--- random.c
+++ random.c
@@ -37,7 +37,7 @@ int main(void)
 		printf("error");
 	}*/
 
-	srand(time(NULL));
+	/*srand(time(NULL));
 	int i = rand() % 3;
 	switch(i)
 	{
@@ -45,7 +45,88 @@ int main(void)
 	case 1: printf("바위\n"); break;
 	case 2: printf("보\n"); break;
 	default:printf("error\n"); break;
+	}*/
+
+	/*int age;
+	printf("age? : ");
+	scanf_s("%d", &age);*/
+	/*if (age >= 8 && age <= 13)
+	{
+		printf("초등학생\n");
 	}
+	else if (age >= 14 && age <= 16)
+	{
+		printf("중학생\n");
+	}
+	else if (age >= 17 && age <= 19)
+	{
+		printf("고등학생\n");
+	}
+	else if (age < 7)
+	{
+		printf("유아");
+	}
+	else
+	{
+		printf("성인\n");
+	}*/
+	/*switch (age)
+	{
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+	case 13: printf("element"); break;
+	case 14:
+	case 15:
+	case 16: printf("middle"); break;
+	case 17:
+	case 18:
+	case 19: printf("high"); break;
+	default: printf("adult");
+		break;
+	}*/
+
+	//up&down
+	srand(time(NULL));
+	int num = rand() % 100 + 1;//1~100
+	printf("숫자 : %d\n", num);
+	int answer = 0;
+	int chance = 5;
+	while (chance > 0) //1:참, 0:거짓
+	{
+		printf("chance %d\n", chance--);
+		printf("answer : ");
+		scanf_s("%d", &answer);
+
+		if (answer > num)
+		{
+			printf("DOWN\n");
+		}
+		else if (answer < num)
+		{
+			printf("UP\n");
+		}
+		else if (answer == num)
+		{
+			printf("CORRECT!\n");
+			break;
+		}
+		else
+		{
+			printf("ERROR\n");
+		}
+
+		if (chance == 0)
+		{
+			printf("남은 기회 없음");
+			break;
+		}
+	}
+		
+
+
 
 
 	return 0;
