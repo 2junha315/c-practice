2ca4751d80100bf6bc3e39fafd8d36f7660e1ce5
junha <junhaha0315@gmail.com>
2024-12-23 16:25:06 +0900

    function practice

--- function.c
+++ function.c
@@ -6,7 +6,16 @@ void function_without_return();
 int fuction_with_return();
 void function_without_params();
 void function_with_params(int num1, int num2, int num3);
-int main(void)
+
+int apple(int total, int ate);//total 에서 ate 먹고 남은 개수 
+
+int add(int num1, int num2);
+int sub(int num1, int num2);
+int mul(int num1, int num2);
+int div(int num1, int num2);
+
+
+int main_function(void)
 {
 //	/*printf("hello world");*/
 //	int num = 2;
@@ -32,10 +41,33 @@ int main(void)
 	//function_without_return();
 	//int ret = fuction_with_return();
 	//p(ret);
-	function_with_params(12, 23, 64);
-	return 0;
+	//function_with_params(12, 23, 64);
+	
 
 	//전달값 없는 함수
+
+
+	//둘다
+	//int ret = apple(5, 2); //사과 5개 중 2개 먹음
+	//printf("사과 5개중 2개를 먹으면 몇개가 남나요? : %d", ret);
+	//printf("사과 %d개 중에 %d개를 먹으면? %d 개가 남아요\n", 10, 4, apple(10, 4));
+
+	//calcuate
+	int num = 2;
+
+	num = add(num, 3);
+	p(num);
+
+	num = sub(num, 1);
+	p(num);
+
+	num = mul(num, 3);
+	p(num);
+
+	num = div(num, 2);
+	p(num);
+
+	return 0;
 }
 
 //정의
@@ -60,5 +92,25 @@ void function_with_params(int num1, int num2, int num3)
 {
 	printf("전달값이 있는 함수, 전달 받은 값은 %d, %d, %d\n", num1, num2, num3);
 }
-
+int apple(int total, int ate)
+{
+	printf("전달값과 반환값이 있는 함수\n");
+	return total - ate;
+}
+////int add(int num1, int num2)
+//{
+//	return num1 + num2;
+//}
+////int sub(int num1, int num2)
+//{
+//	return num1 - num2;
+//}
+////int mul(int num1, int num2)
+//{
+//	return num1 * num2;
+//}
+////int div(int num1, int num2)
+//{
+//	return num1 / num2;
+//}
 //반환형 함수이름(전달값){동작}
